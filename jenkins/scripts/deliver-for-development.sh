e246172491f2bb2f1069b89dbca7c7c46b7535ea
Hangwee <lee_hangwee@hotmail.com>
2020-03-08 22:38:45 +0800

    npm to yarn

--- jenkins/scripts/deliver-for-development.sh
+++ jenkins/scripts/deliver-for-development.sh
@@ -9,7 +9,7 @@ echo 'is followed by another command that retrieves the process ID (PID) value'
 echo 'of the previously run process (i.e. "npm start") and writes this value to'
 echo 'the file ".pidfile".'
 set -x
-npm start &
+yarn start &
 sleep 1
 echo $! > .pidfile
 set +x
