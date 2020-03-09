f28abc170029b5de894091872fd91945f78dcc1f
Hangwee <lee_hangwee@hotmail.com>
2020-03-09 17:06:35 +0800

    new profile pic

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -9,7 +9,7 @@ export default class Sidebar extends Component {
           <aside id="colorlib-aside" className="border js-fullheight">
             <div className="text-center">
               <div className="author-img" style={{backgroundImage: 'url(images/linkedin_photo.jpeg)'}} />
-              <h1 id="colorlib-logo"><a href="index.html">Dhruv Barochiya</a></h1>
+              <h1 id="colorlib-logo"><a href="index.html">Lee Hang Wee</a></h1>
               <span className="email"><i className="icon-mail"></i> leehangwee@gmail.com</span>
             </div>
             <nav id="colorlib-main-menu" role="navigation" className="navbar">
